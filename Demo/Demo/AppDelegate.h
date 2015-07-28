ac86e01e25d96b89bd03a4a408f070a3f3272a28
朱炳程 <cheng4741@qq.com>
2015-07-28 17:29:31 +0800

    调整文件目录，增加demo工程

---
+++ Demo/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by zhubch on 15/7/28.
+//  Copyright (c) 2015年 zhubch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
